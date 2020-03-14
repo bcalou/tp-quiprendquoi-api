d244bb3130b19e3e434af105fe5931408ae408a6
bcalou <bastien.calou@gmail.com>
2020-03-14 20:06:41 +0100

    [api] item CRUD

--- models/party.js
+++ models/party.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
+const itemSchema = require('./item');
 
 const partySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  author: {
-    type: String,
-    required: true,
-  },
+  name: { type: String, required: true },
+  author: { type: String, required: true },
   date: Date,
+  items: [itemSchema],
 });
 
-module.exports = mongoose.model('Party', partySchema);
+module.exports = partySchema;
