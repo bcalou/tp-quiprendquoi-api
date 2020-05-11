9ff1a9171f53ea8c54ac322952587ee02c266498
bcalou <bastien.calou@gmail.com>
2020-05-11 17:06:33 +0200

    [api]  allow all methods

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ mongoose.connect(mongoUrl, {
 
 app.use(bodyParser());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
